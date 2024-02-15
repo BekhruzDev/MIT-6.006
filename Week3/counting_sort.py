a6a80c31f4fe475a5f8c954bebf08db7906aed3e
Bekhruz <bekhruzabdullakhujaevpro@gmail.com>
2024-02-15 22:38:45 +0500

    impl countin sort using cumulative sums

--- Week3/counting_sort.py
+++ Week3/counting_sort.py
@@ -1,3 +1,6 @@
+import random
+
+
 def counting_sort(A):
     '''
     The same as Direct Access Array sort and can also handle duplicate keys using chains at the indices, just like hash tables.
@@ -21,8 +24,46 @@ def counting_sort(A):
             
     return A
 
-#TODO: implement the couting sort using cumulative sums tmwr.
+def counting_sort_cumulative(A):
+    '''
+    The same as counting_sort(), but cumulates the number of keys at each index of DAA.
+    Then, orders the keys based on the values at DAA[key] and decrements it's number, DAA[key] -= 1
+    Overall, runtime = O(u + n)
+    '''
+    u = 1 + max([x.key for x in A])  #O(n) find the maximum key
+    D = [0] * u                      #O(u) init Direct Access Array
+    
+    for x in A:                      #O(n) count occurrences
+        D[x.key] += 1
+    
+    for i in range(1,u):             #O(u) cumulative sums
+        D[i] += D[i - 1]
+    
+    for x in list(reversed(A)):      #O(n) reorder the items at A, NOTE: WE ARE REVERSING THE GIVEN A TO MAKE THE SORT STABLE
+        A[D[x.key] - 1] = x
+        D[x.key] -= 1
+    
+    return A
 
+class Item:
+    def __init__(self, key, value) -> None:
+        self.key = key
+        self.value = value
+    def __str__(self) -> str:
+        return f"({self.key}, {self.value})"
+    
+        
+my_list = [Item(random.randint(0, 100), f"Value {i+1}") for i in range(20)]
+for item in my_list:
+    print(item)
+
+print("\n================== COUNTING SORT V1 =================================\n")
+for item in counting_sort(my_list):
+    print(item)
+    
+print("\n================== COUNTING SORT V2 =================================\n")
+for item in counting_sort_cumulative(my_list):
+    print(item)
     
 
     
